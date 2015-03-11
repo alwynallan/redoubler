9568f5bcef602c48696c8d2a7de1c8098b2546be
Peter Allan <alwynallan@gmail.com>
2015-03-11 08:42:48 -0400

    Cleaned whiten

--- host_linux/whiten.c
+++ host_linux/whiten.c
@@ -3,13 +3,14 @@
 #include "ecrypt-sync.h"
 
 #define KEY_ROUNDS 5 // one or more
+#define EXPANSION 1 // more bits, same entropy, set to none
 
 int main()
 {
   ECRYPT_ctx x;
-  u8 key[32]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-              0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-  u8 iv[8]={0,0,0,0,0,0,0,0};
+  u8 key[32]={31,41,59,26,53,58,97,93,23,84,62,64,33,83,27,95,
+               2,88,41,97,16,93,99,37,51, 5,82, 9,74,94,45,92}; // pi
+  u8 iv[8]={27,18,28,18,28,45,90,45}; // e
   u8 buf[64], obuf[64];
   int i, j, bytes;
 
@@ -25,6 +26,10 @@ int main()
     ECRYPT_encrypt_bytes(&x,buf,obuf,bytes);
     fwrite(obuf,1,bytes,stdout);
     // should re-initialize iv every 195 million years for REDOUBLER
+	for(i=1; i<EXPANSION; i++){
+      ECRYPT_keystream_bytes(&x,obuf,64);
+      fwrite(obuf,1,64,stdout);
+	}
   }
   return 0;
 }
