dc2129b9a5757fe898845ada78400e051706d7bb
Peter Allan <alwynallan@gmail.com>
2015-03-11 05:18:51 -0400

    Format a comment

--- host_linux/whiten.c
+++ host_linux/whiten.c
@@ -1,3 +1,4 @@
+// Peter Allan 2015
 #include <stdio.h>
 #include "ecrypt-sync.h"
 
@@ -5,25 +6,25 @@
 
 int main()
 {
-    ECRYPT_ctx x;
-	u8 key[32]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-	            0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-    u8 iv[8]={0,0,0,0,0,0,0,0};
-    u8 buf[64], obuf[64];
-	int i, j, bytes;
-	
-	for(j=0;j<KEY_ROUNDS;j++){
-	  fread(buf,1,40,stdin);
-	  for(i=0;i<32;i++) key[i] ^= buf[i];
-	  for(i=0;i<8;i++) iv[i] ^= buf[i+32];
-	}
-	ECRYPT_keysetup(&x,key,256,64);
-	ECRYPT_ivsetup(&x,iv);
-	while(!feof(stdin)){
-	  bytes = fread(buf, 1, 64, stdin);
-	  ECRYPT_encrypt_bytes(&x,buf,obuf,bytes);
-	  fwrite(obuf,1,bytes,stdout);
-	  // should re-initialize iv every 195 million years for REDOUBLER
-	}
-	return 0;
+  ECRYPT_ctx x;
+  u8 key[32]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+              0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+  u8 iv[8]={0,0,0,0,0,0,0,0};
+  u8 buf[64], obuf[64];
+  int i, j, bytes;
+
+  for(j=0;j<KEY_ROUNDS;j++){
+    fread(buf,1,40,stdin);
+    for(i=0;i<32;i++) key[i] ^= buf[i];
+    for(i=0;i<8;i++) iv[i] ^= buf[i+32];
+  }
+  ECRYPT_keysetup(&x,key,256,64);
+  ECRYPT_ivsetup(&x,iv);
+  while(!feof(stdin)){
+    bytes = fread(buf, 1, 64, stdin);
+    ECRYPT_encrypt_bytes(&x,buf,obuf,bytes);
+    fwrite(obuf,1,bytes,stdout);
+    // should re-initialize iv every 195 million years for REDOUBLER
+  }
+  return 0;
 }
